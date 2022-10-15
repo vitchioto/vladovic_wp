7edf8f064b25c611b29eba1238304a9bdb58ae8c
Vladimir Vladovic <vladimir.vladovic@entaingroup.com>
2022-10-15 12:35:51 +0200

    Fix footer

--- footer.php
+++ footer.php
@@ -1,12 +1,24 @@
   </main>
   <footer class="footer">
     <section class="footer__content">
-      <a href="">
-        LinkedIn
-      </a>
-      <a href="">
-        Github
-      </a>
+      <div class="footer__links">
+        <a
+          href="https://www.linkedin.com/in/vladim%C3%ADr-vladovi%C4%8D-200b11122"
+          class="footer__link"
+          rel="noreferrer"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/vitchioto"
+          class="footer__link"
+          rel="noreferrer" 
+          target="_blank"
+        >
+          Github
+        </a>
+      </div>
       <div class="copyright">
         Ⓒ Vladimir Vladovic 2022
       </div>
