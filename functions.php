801d14bf66a8b1f86c2dd375903bf72316de795b
Vladimir Vladovic <vladimir.vladovic@entaingroup.com>
2022-10-23 18:32:30 +0200

    Remove WP block-library styles

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ if (!function_exists('vv_basic_setup')) {
 add_action('after_setup_theme', 'vv_basic_setup');
 
 function vv_basic_enqueue_scripts() {
+  wp_dequeue_style('wp-block-library');
   wp_enqueue_style('style', get_stylesheet_uri(), array(), '0.0.2');
   wp_enqueue_script( 'custom_js', get_template_directory_uri().'/index.js', array( 'jquery' ), '0.0.2', true );
 }
